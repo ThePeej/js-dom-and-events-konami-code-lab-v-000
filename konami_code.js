71559c58294d7d3e3a6ecb75fc63dd9226458b68
PJ Frias <PeejFrias@Gmail.com>
2018-03-16 10:02:24 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,32 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', keyPress)
+}
 
+let i = 0;
+let check = [];
+
+function keyPress(e) {
+    key = parseInt(e.which || e.detail);
+    if (key === code[i]){
+      check.push(key);
+      ++i;
+      console.log(check)
+      checkCheats()
+    } else {
+      restart()
+    }
+}
+
+function checkCheats(){
+  if (check.length === 10){
+    alert("HAXXORZZZ")
+  }
+}
+
+function restart(){
+  i = 0;
+  check = [];
+  console.log("Start Over!")
 }
